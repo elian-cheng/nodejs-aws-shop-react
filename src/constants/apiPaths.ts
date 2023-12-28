bf8103cacbc577c8a159db55e616088fbb8879d4
Olga Chernega <eliang.cheng@gmail.com>
2023-12-28 18:39:58 +0200

    feat: change API path to EBS URL

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://d0jroimskb.execute-api.eu-north-1.amazonaws.com/products",
-  order: "https://eaq3zyv779.execute-api.eu-north-1.amazonaws.com/dev/api",
+  order: "http://elian-rss-cart-api-dev.eu-north-1.elasticbeanstalk.com/api",
   import: "https://2atwhq6z3f.execute-api.eu-north-1.amazonaws.com",
   bff: "https://d0jroimskb.execute-api.eu-north-1.amazonaws.com",
-  cart: "https://eaq3zyv779.execute-api.eu-north-1.amazonaws.com/dev/api"
+  cart: "http://elian-rss-cart-api-dev.eu-north-1.elasticbeanstalk.com/api"
 };
 
 export default API_PATHS;
