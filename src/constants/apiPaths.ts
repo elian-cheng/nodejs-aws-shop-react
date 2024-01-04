e39ed0f1f359be47e321a3b9144816ae8f7fbff1
Olga Chernega <eliang.cheng@gmail.com>
2024-01-04 14:32:40 +0200

    feat: add https proxy API to beanstalk

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://d0jroimskb.execute-api.eu-north-1.amazonaws.com/products",
-  order: "http://elian-rss-cart-api-dev.eu-north-1.elasticbeanstalk.com/api",
+  order: "https://73lvboyfnc.execute-api.eu-north-1.amazonaws.com/api",
   import: "https://2atwhq6z3f.execute-api.eu-north-1.amazonaws.com",
   bff: "https://d0jroimskb.execute-api.eu-north-1.amazonaws.com",
-  cart: "http://elian-rss-cart-api-dev.eu-north-1.elasticbeanstalk.com/api"
+  cart: "https://73lvboyfnc.execute-api.eu-north-1.amazonaws.com/api"
 };
 
 export default API_PATHS;
